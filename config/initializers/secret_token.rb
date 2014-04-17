2bdaae8a67602c114e2483cc0d3bdf354a0f4814
xxhuang <tiancaihxx@hotmail.com>
2014-04-17 10:49:59 -0500

    Finished the User model and login part

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditOnRails::Application.config.secret_key_base = 'c441c15d79e890f3f5cc2212fea1d2b922b71860eb97f5854e9cd4696e8c2ffbbf29539492ea1d9569999876d1628f8a8cb1abbea6c0756487b1984fe9a3eeb6'
