97d150ec6039f52c7d395cfc3464960f958ae26c
xxhuang <tiancaihxx@hotmail.com>
2014-04-28 14:25:41 -0500

    add endless scrolling to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
 		@link = Link.all
 		@links = Link.paginate(page: params[:page], per_page: 10) 	
 		respond_to do |format|
-      		format.html
-      		format.js # add this line for your js template
-    	end	
+  			format.html
+  			format.js
+  		end
 	end
 end
